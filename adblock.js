60dbbc422d650cc2a71ce7be6973234dc7b06566
abaanfahim207-ops <abaanfahim207@gmail.com>
2026-07-25 17:58:00 +0500

    Create adblock.js
    Signed-off-by: abaanfahim207-ops <abaanfahim207@gmail.com>

---
+++ adblock.js
@@ -0,0 +1,13 @@
+// Hides common ad elements dynamically as the game loads
+const hideAds = () => {
+  const adSelectors = [
+    '.ad', '.ads', '#ad-container', 'iframe[src*="googleads"]',
+    'iframe[src*="doubleclick"]', '[id*="ad-"]'
+  ];
+  adSelectors.forEach(selector => {
+    document.querySelectorAll(selector).forEach(el => el.remove());
+  });
+};
+
+document.addEventListener('DOMContentLoaded', hideAds);
+setInterval(hideAds, 2000);
